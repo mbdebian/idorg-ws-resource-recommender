657a71f2f6ccee250ee0e4dce6fb9a174f72817b
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-05-08 21:52:17 +0100

    Experimenting with GO, changes checkpoint

---
+++ src/greet/greeting.go
@@ -0,0 +1,12 @@
+/*
+ * -----------------------------------------------------------
+ * File Created: 2020-05-08T15:09:15.776Z
+ * Author: Manuel Bernal Llinares <mbdebian@gmail.com>
+ * -----------------------------------------------------------
+ */
+package greet
+
+const (
+	myGreet = "Internal greeting"
+	MyGreet = "Exported greeting"
+)
