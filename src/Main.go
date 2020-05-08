fe524a6f06b14717e86b3a0a94ff71f7f2772c7e
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-05-08 13:14:20 +0100

    sample Main in GO

---
+++ src/Main.go
@@ -0,0 +1,13 @@
+/*
+ * -----------------------------------------------------------
+ * File Created: 2020-05-08T13:11:32.936Z
+ * Author: Manuel Bernal Llinares <mbdebian@gmail.com>
+ * -----------------------------------------------------------
+ */
+import (
+	"fmt"
+)
+
+func main()  {
+	fmt.println("Hello everybody from my first GO micro")
+}
