af87a8355a889d8895af8f8199294821bad3ece7
Manuel Bernal Llinares <mbdebian@gmail.com>
2020-05-08 13:41:59 +0100

    refactoring

--- src/org.identifiers.ws.resource-recommender/Main.go
+++ src/org.identifiers.ws.resource-recommender/Main.go
@@ -4,10 +4,12 @@
  * Author: Manuel Bernal Llinares <mbdebian@gmail.com>
  * -----------------------------------------------------------
  */
+package mbdebian/idorg/ws/resourcerecommender
+
 import (
 	"fmt"
 )
 
-func main()  {
-	fmt.println("Hello everybody from my first GO micro")
+func main() {
+	fmt.Println("Hello everybody from my first GO microservice")
 }
